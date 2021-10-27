a0acf9fe1b3b869ab3765ec8067d4055272de8ba
Matheus Carvalho <matheuscarva@hotmail.com>
2021-10-27 20:08:12 -0300

    criado modulo do usuario

--- interface/src/app/app-routing.module.ts
+++ interface/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{
   path: 'company',
   loadChildren: () => import('./modules/company/company.module').then(mod => mod.CompanyModule)
-}];
+},
+  { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
