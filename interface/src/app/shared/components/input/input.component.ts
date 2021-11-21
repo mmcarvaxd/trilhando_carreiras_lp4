0268f3c2c1a040f49490ba70184dfce6278a756a
Matheus Carvalho <matheuscarva@hotmail.com>
2021-11-21 19:47:20 -0300

    adiciona disable no input

--- interface/src/app/shared/components/input/input.component.ts
+++ interface/src/app/shared/components/input/input.component.ts
@@ -10,6 +10,7 @@ export class InputComponent implements OnInit {
   @Input() fieldType = 'text'
   @Input() fieldValue: any
   @Input() isValid: boolean = true
+  @Input() isDisabled: boolean = false
   @Output() fieldValueChange: EventEmitter<any> = new EventEmitter<any>()
 
   constructor() { }
