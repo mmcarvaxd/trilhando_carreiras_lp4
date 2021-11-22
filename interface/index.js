725cfe3937b783cc7fc233694f8446ea8132c0db
Matheus Carvalho <matheuscarva@hotmail.com>
2021-11-21 22:59:03 -0300

    add server

--- interface/index.js
+++ interface/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/interface'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/interface/'}),
+    res.sendFile('index.html', {root: 'dist/interface/' }),
 );
 
 // Start the app by listening on the default Heroku port
